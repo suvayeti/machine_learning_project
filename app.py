ed6b8f11e45db7cf662b78dcc5554eff0cb8ee30
Suvash Sharma <suvayeti@gmail.com>
2022-06-12 03:53:43 -0400

    name added

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return ("Starting Machine Learning Project")
+    return ("<h1>Starting Machine Learning Project by suvash</h1>")
 
 if __name__=="__main__":
     app.run(debug=True)
